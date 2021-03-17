122e45e0578f2543331caece6c4e2921fa97bd8c
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-03-17 15:56:56 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Input Date Picker React</h1>
+        </div>
+    );
+}
+
+export default hot(App);
