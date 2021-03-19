2c997dfd59a1933e5525ecceeeacad832d6f0827
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-03-19 11:23:20 +0100

    Close dropdown on click outside

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export const Dropdown = (props) => {
     const { trigger } = props;
 
     const [open, setOpen] = useState(false);
+    const dropdownDom = useRef();
 
     const dropdown = (
         <div className="dropdown">
@@ -13,8 +14,21 @@ export const Dropdown = (props) => {
 
     const handleOpen = () => setOpen(!open);
 
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (dropdownDom.current && !dropdownDom.current.contains(e.target)) {
+                setOpen(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+    });
+
     return (
-        <div className="dropdown-parent">
+        <div className="dropdown-parent" ref={dropdownDom}>
             <div onClick={handleOpen}>
                 {trigger}
             </div>
